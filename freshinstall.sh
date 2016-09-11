26fcc0f8ba92e614d8443111d4d16dc42937c79d
Albert Park <albe.park@gmail.com>
2016-09-11 02:24:27 -0700

    updated sofware versions, cleanup w/ variables and fewer apt updates

--- freshinstall.sh
+++ freshinstall.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Software Versions
+sublime=sublime-text_build-3114_amd64.deb
+slack=slack-desktop-2.1.2-amd64.deb
+vscode=index.html?LinkID=760868
+
 # Initialize
 sudo apt update &&
 sudo apt -y upgrade && 
@@ -13,47 +18,41 @@ sudo add-apt-repository -y ppa:ubuntu-desktop/ubuntu-make &&
 sudo add-apt-repository -y ppa:nilarimogard/webupd8 &&
 sudo add-apt-repository -y ppa:noobslab/themes &&
 
+# Spotify
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys BBEBDCB318AD50EC6865090613B00F1FD2C19886 &&
+echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list &&
+
 # Install programs
 sudo apt update &&
-sudo apt install -y nautilus-dropbox virtualbox chromium-browser keepassx unity-tweak-tool htop powertop ubuntu-restricted-extras pepperflashplugin-nonfree tlp ubuntu-make albert dconf-editor nodejs-legacy screenfetch psensor &&
-
+sudo apt install -y nautilus-dropbox spotify-client virtualbox chromium-browser keepassx unity-tweak-tool htop powertop ubuntu-restricted-extras pepperflashplugin-nonfree tlp ubuntu-make albert dconf-editor nodejs-legacy screenfetch psensor &&
 
 # Meteor
 # curl https://install.meteor.com/ | sh &&
 
 # Sublime Text
-wget https://download.sublimetext.com/sublime-text_build-3114_amd64.deb &&
-sudo dpkg -i sublime-text_build-3114_amd64.deb &&
+wget https://download.sublimetext.com/$sublime &&
+sudo dpkg -i $sublime &&
 
 # Slack 
-wget https://downloads.slack-edge.com/linux_releases/slack-desktop-2.1.0-amd64.deb &&
-sudo dpkg -i slack-desktop-2.1.0-amd64.deb && 
+wget https://downloads.slack-edge.com/linux_releases/$slack &&
+sudo dpkg -i $slack && 
+
+# Visual Studio Code
+wget https://go.microsoft.com/fwlink/?LinkID=760868 &&
+sudo dpkg -i $vscode &&
 
 # Ghost desktop
 wget http://desktop-updates.ghost.org/download/linux &&
 sudo dpkg -i linux
 
-# Spotify
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys BBEBDCB318AD50EC6865090613B00F1FD2C19886 &&
-echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list &&
-sudo apt update &&
-# sudo apt -y install spotify-client-0.9.17 &&
-sudo apt -y install spotify-client &&
-
 # Start software
 sudo tlp start &&
-
-# Ubuntu Make software
-# umake ide visual-studio-code &&
-wget https://go.microsoft.com/fwlink/?LinkID=760868 &&
-sudo dpkg -i code_1.4.0-1470329130_amd64.deb &&
 
 # NVM
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.1/install.sh | bash &&
 
 # Remove debs
-sudo rm sublime-text_build-3114_amd64.deb &&
-sudo rm slack-desktop-2.1.0-amd64.deb &&
+sudo rm $sublime $slack $vscode &&
 
 sudo apt update && 
 sudo apt -y upgrade && 
